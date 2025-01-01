f63b78238cdad4d97fff3a534eeec8ea00df6f4a
Paul Rodwell <paul.rodwell@btinternet.com>
2025-01-01 11:41:21 +0000

    update build

---
+++ scripts/update-authors.cjs
@@ -0,0 +1,27 @@
+const gitAuthors = require('grunt-git-authors')
+
+// list of contributers from prior the split out of Smallest Federated Wiki repo.
+const priorAuthors = [
+  'Ward Cunningham <ward@c2.com>',
+  'Stephen Judkins <stephen.judkins@gmail.com>',
+  'Nick Niemeir <nick.niemeir@gmail.com>',
+]
+
+gitAuthors.updatePackageJson({ priorAuthors: priorAuthors, order: 'date' }, error => {
+  if (error) {
+    console.log('Error: ', error)
+  }
+})
+
+gitAuthors.updateAuthors(
+  {
+    priorAuthors: priorAuthors,
+  },
+  (error, filename) => {
+    if (error) {
+      console.log('Error: ', error)
+    } else {
+      console.log(filename, 'updated')
+    }
+  },
+)
